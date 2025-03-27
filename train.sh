3ba8f0c44a206b26b04a87f1a9a54435e08b379a
Syrinx <syrinx_w@163.com>
2025-03-27 12:25:01 +0000

    2.2 18_all 18_re_cj 50 50512

--- train.sh
+++ train.sh
@@ -16,36 +16,41 @@
 # --save-dir logs/TEST-mobilenet_v3_small
 
 # 2.1
-for random_erase in "" "--random-erase"; do
-    for color_jitter in "" "--color-jitter"; do
-        for color_aug in "" "--color-aug"; do
+STUDENT_ID=zw00953 STUDENT_NAME="Ziyu Wang" python main.py \
+-s veri \
+-t veri \
+-a resnet18 \
+--root datasets \
+--height 224 \
+--width 224 \
+--optim amsgrad \
+--lr 0.0003 \
+--max-epoch 10 \
+--stepsize 20 40 \
+--train-batch-size 64 \
+--test-batch-size 100 \
+--random-erase \
+--color-jitter \
+--save-dir logs/2.2-resnet18-randomerase-colorjitter
 
-            # exprriment name generation
-            EXP_NAME="2.2-resnet34_fc512"
-            [ ! -z "$random_erase" ] && EXP_NAME+="-randomerase"
-            [ ! -z "$color_jitter" ] && EXP_NAME+="-colorjitter"
-            [ ! -z "$color_aug" ] && EXP_NAME+="-coloraug"
+STUDENT_ID=zw00953 STUDENT_NAME="Ziyu Wang" python main.py \
+-s veri \
+-t veri \
+-a resnet18 \
+--root datasets \
+--height 224 \
+--width 224 \
+--optim amsgrad \
+--lr 0.0003 \
+--max-epoch 10 \
+--stepsize 20 40 \
+--train-batch-size 64 \
+--test-batch-size 100 \
+--random-erase \
+--color-jitter \
+--color-aug \
+--save-dir logs/2.2-resnet18-randomerase-colorjitter-coloraug
 
-            echo "Running experiment: $EXP_NAME"
-
-            STUDENT_ID=zw00953 STUDENT_NAME="Ziyu Wang" python main.py \
-                -s veri \
-                -t veri \
-                -a resnet34_fc512 \
-                --root datasets \
-                --height 224 \
-                --width 224 \
-                --optim amsgrad \
-                --lr 0.0003 \
-                --max-epoch 10 \
-                --stepsize 20 40 \
-                --train-batch-size 64 \
-                --test-batch-size 100 \
-                --save-dir logs/$EXP_NAME \
-                $random_erase $color_jitter $color_aug 
-        done
-    done
-done
 
 for random_erase in "" "--random-erase"; do
     for color_jitter in "" "--color-jitter"; do
