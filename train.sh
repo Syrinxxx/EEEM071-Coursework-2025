adcc9732d63662e16004c699ae4d79319f009bdf
Syrinx <syrinx_w@163.com>
2025-03-27 12:28:04 +0000

    split 34 to veri2

--- train.sh
+++ train.sh
@@ -83,34 +83,34 @@ for random_erase in "" "--random-erase"; do
     done
 done
 
-for random_erase in "" "--random-erase"; do
-    for color_jitter in "" "--color-jitter"; do
-        for color_aug in "" "--color-aug"; do
+# for random_erase in "" "--random-erase"; do
+#     for color_jitter in "" "--color-jitter"; do
+#         for color_aug in "" "--color-aug"; do
 
-            # exprriment name generation
-            EXP_NAME="2.2-resnet34"
-            [ ! -z "$random_erase" ] && EXP_NAME+="-randomerase"
-            [ ! -z "$color_jitter" ] && EXP_NAME+="-colorjitter"
-            [ ! -z "$color_aug" ] && EXP_NAME+="-coloraug"
+#             # exprriment name generation
+#             EXP_NAME="2.2-resnet34"
+#             [ ! -z "$random_erase" ] && EXP_NAME+="-randomerase"
+#             [ ! -z "$color_jitter" ] && EXP_NAME+="-colorjitter"
+#             [ ! -z "$color_aug" ] && EXP_NAME+="-coloraug"
 
-            echo "Running experiment: $EXP_NAME"
+#             echo "Running experiment: $EXP_NAME"
 
-            STUDENT_ID=zw00953 STUDENT_NAME="Ziyu Wang" python main.py \
-                -s veri \
-                -t veri \
-                -a resnet34 \
-                --root datasets \
-                --height 224 \
-                --width 224 \
-                --optim amsgrad \
-                --lr 0.0003 \
-                --max-epoch 10 \
-                --stepsize 20 40 \
-                --train-batch-size 64 \
-                --test-batch-size 100 \
-                --save-dir logs/$EXP_NAME \
-                $random_erase $color_jitter $color_aug 
-        done
-    done
-done
+#             STUDENT_ID=zw00953 STUDENT_NAME="Ziyu Wang" python main.py \
+#                 -s veri \
+#                 -t veri \
+#                 -a resnet34 \
+#                 --root datasets \
+#                 --height 224 \
+#                 --width 224 \
+#                 --optim amsgrad \
+#                 --lr 0.0003 \
+#                 --max-epoch 10 \
+#                 --stepsize 20 40 \
+#                 --train-batch-size 64 \
+#                 --test-batch-size 100 \
+#                 --save-dir logs/$EXP_NAME \
+#                 $random_erase $color_jitter $color_aug 
+#         done
+#     done
+# done
 
