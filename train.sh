cb62bc9c50766e2557bb9c57414a7f8ecb8badc1
Syrinx <syrinx_w@163.com>
2025-03-27 01:27:14 +0000

    2.2.2 18 18512 34 vgg

--- train.sh
+++ train.sh
@@ -1,51 +1,145 @@
 #!/bin/bash
 
-STUDENT_ID=zw00953 STUDENT_NAME="Ziyu Wang" python main.py \
--s veri \
--t veri \
--a resnet50 \
---root datasets \
---height 224 \
---width 224 \
---optim amsgrad \
---lr 0.0003 \
---max-epoch 10 \
---stepsize 20 40 \
---train-batch-size 64 \
---test-batch-size 100 \
---random-erase \
---color-jitter \
---save-dir logs/2.2-resnet50-randomerase-colorjitter-veri
+# STUDENT_ID=zw00953 STUDENT_NAME="Ziyu Wang" python main.py \
+# -s veri \
+# -t veri \
+# -a resnet50 \
+# --root datasets \
+# --height 224 \
+# --width 224 \
+# --optim amsgrad \
+# --lr 0.0003 \
+# --max-epoch 10 \
+# --stepsize 20 40 \
+# --train-batch-size 64 \
+# --test-batch-size 100 \
+# --random-erase \
+# --color-jitter \
+# --save-dir logs/2.2-resnet50-randomerase-colorjitter-veri
 
-# # 2.1
-# for random_erase in "" "--random-erase"; do
-#     for color_jitter in "" "--color-jitter"; do
-#         for color_aug in "" "--color-aug"; do
+# 2.1
+for random_erase in "" "--random-erase"; do
+    for color_jitter in "" "--color-jitter"; do
+        for color_aug in "" "--color-aug"; do
 
-#             # exprriment name generation
-#             EXP_NAME="2.1-mobilenet_v3_small"
-#             [ ! -z "$random_erase" ] && EXP_NAME+="-randomerase"
-#             [ ! -z "$color_jitter" ] && EXP_NAME+="-colorjitter"
-#             [ ! -z "$color_aug" ] && EXP_NAME+="-coloraug"
+            # exprriment name generation
+            EXP_NAME="2.2-resnet18"
+            [ ! -z "$random_erase" ] && EXP_NAME+="-randomerase"
+            [ ! -z "$color_jitter" ] && EXP_NAME+="-colorjitter"
+            [ ! -z "$color_aug" ] && EXP_NAME+="-coloraug"
 
-#             echo "Running experiment: $EXP_NAME"
+            echo "Running experiment: $EXP_NAME"
 
-#             STUDENT_ID=zw00953 STUDENT_NAME="Ziyu Wang" python main.py \
-#                 -s veri \
-#                 -t veri \
-#                 -a mobilenet_v3_small \
-#                 --root datasets \
-#                 --height 224 \
-#                 --width 224 \
-#                 --optim amsgrad \
-#                 --lr 0.0003 \
-#                 --max-epoch 10 \
-#                 --stepsize 20 40 \
-#                 --train-batch-size 64 \
-#                 --test-batch-size 100 \
-#                 --save-dir logs/$EXP_NAME \
-#                 $random_erase $color_jitter $color_aug 
-#         done
-#     done
-# done
+            STUDENT_ID=zw00953 STUDENT_NAME="Ziyu Wang" python main.py \
+                -s veri \
+                -t veri \
+                -a resnet18 \
+                --root datasets \
+                --height 224 \
+                --width 224 \
+                --optim amsgrad \
+                --lr 0.0003 \
+                --max-epoch 10 \
+                --stepsize 20 40 \
+                --train-batch-size 64 \
+                --test-batch-size 100 \
+                --save-dir logs/$EXP_NAME \
+                $random_erase $color_jitter $color_aug 
+        done
+    done
+done
+
+for random_erase in "" "--random-erase"; do
+    for color_jitter in "" "--color-jitter"; do
+        for color_aug in "" "--color-aug"; do
+
+            # exprriment name generation
+            EXP_NAME="2.2-resnet18_fc512"
+            [ ! -z "$random_erase" ] && EXP_NAME+="-randomerase"
+            [ ! -z "$color_jitter" ] && EXP_NAME+="-colorjitter"
+            [ ! -z "$color_aug" ] && EXP_NAME+="-coloraug"
+
+            echo "Running experiment: $EXP_NAME"
+
+            STUDENT_ID=zw00953 STUDENT_NAME="Ziyu Wang" python main.py \
+                -s veri \
+                -t veri \
+                -a resnet18_fc512 \
+                --root datasets \
+                --height 224 \
+                --width 224 \
+                --optim amsgrad \
+                --lr 0.0003 \
+                --max-epoch 10 \
+                --stepsize 20 40 \
+                --train-batch-size 64 \
+                --test-batch-size 100 \
+                --save-dir logs/$EXP_NAME \
+                $random_erase $color_jitter $color_aug 
+        done
+    done
+done
+
+for random_erase in "" "--random-erase"; do
+    for color_jitter in "" "--color-jitter"; do
+        for color_aug in "" "--color-aug"; do
+
+            # exprriment name generation
+            EXP_NAME="2.2-resnet34"
+            [ ! -z "$random_erase" ] && EXP_NAME+="-randomerase"
+            [ ! -z "$color_jitter" ] && EXP_NAME+="-colorjitter"
+            [ ! -z "$color_aug" ] && EXP_NAME+="-coloraug"
+
+            echo "Running experiment: $EXP_NAME"
+
+            STUDENT_ID=zw00953 STUDENT_NAME="Ziyu Wang" python main.py \
+                -s veri \
+                -t veri \
+                -a resnet34 \
+                --root datasets \
+                --height 224 \
+                --width 224 \
+                --optim amsgrad \
+                --lr 0.0003 \
+                --max-epoch 10 \
+                --stepsize 20 40 \
+                --train-batch-size 64 \
+                --test-batch-size 100 \
+                --save-dir logs/$EXP_NAME \
+                $random_erase $color_jitter $color_aug 
+        done
+    done
+done
+
+for random_erase in "" "--random-erase"; do
+    for color_jitter in "" "--color-jitter"; do
+        for color_aug in "" "--color-aug"; do
+
+            # exprriment name generation
+            EXP_NAME="2.2-vgg16"
+            [ ! -z "$random_erase" ] && EXP_NAME+="-randomerase"
+            [ ! -z "$color_jitter" ] && EXP_NAME+="-colorjitter"
+            [ ! -z "$color_aug" ] && EXP_NAME+="-coloraug"
+
+            echo "Running experiment: $EXP_NAME"
+
+            STUDENT_ID=zw00953 STUDENT_NAME="Ziyu Wang" python main.py \
+                -s veri \
+                -t veri \
+                -a vgg16 \
+                --root datasets \
+                --height 224 \
+                --width 224 \
+                --optim amsgrad \
+                --lr 0.0003 \
+                --max-epoch 10 \
+                --stepsize 20 40 \
+                --train-batch-size 64 \
+                --test-batch-size 100 \
+                --save-dir logs/$EXP_NAME \
+                $random_erase $color_jitter $color_aug 
+        done
+    done
+done
+
 
